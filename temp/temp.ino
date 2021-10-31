dad3abda3fa19e44c2336672481aa4288f52da25
Soumen Sikder Shuvo <soumen123898@gmail.com>
2021-10-31 15:27:17 +0600

    Update temp.ino

--- temp/temp.ino
+++ temp/temp.ino
@@ -1,9 +1,21 @@
 void setup() {
-  // put your setup code here, to run once:
-
+  // put your setup code here, to run once
+  Serial.begin(9600);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  // put your main code here, to run repeatedly
+  int a = 1;
+  Serial.println(fact(a));
+  delay(100);
+  a++;
+}
 
+int fact(int n){
+  if(n>1){
+    return n* fact(n-1);
+  }
+  else{
+    return 1;
+  }
 }
